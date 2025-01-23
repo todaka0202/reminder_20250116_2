3d5e96295026215d2cb9a7bbf6a5dfaa28bcdb87
todaka0202 <yizaihugao@gmail.com>
2025-01-23 11:08:48 +0900

    aaa

--- reminder/urls.py
+++ reminder/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = "reminder"
+
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("index/", views.IndexView.as_view(), name="index"),
 ]
--- reminder/views.py
+++ reminder/views.py
@@ -1,5 +1,11 @@
-from django.http import HttpResponse
+# from django.shortcuts import render
+# from django.shortcuts import HttpResponsePermanentRedirect
+# from django.shortcuts import HttpResponseRedirect
+# from django.shortcuts import reverse
+from django.views import generic
+
+# from .models import Schedule
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+class IndexView(generic.TemplateView):
+    template_name = "reminder/index.html"
