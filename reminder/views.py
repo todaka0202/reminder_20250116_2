0bc020382e78b62852e934c44f26db3022f0a1b8
todaka0202 <yizaihugao@gmail.com>
2025-01-23 11:52:10 +0900

    i

--- reminder/views.py
+++ reminder/views.py
@@ -1,11 +1,20 @@
 # from django.shortcuts import render
-# from django.shortcuts import HttpResponsePermanentRedirect
+from django.shortcuts import HttpResponsePermanentRedirect
+
 # from django.shortcuts import HttpResponseRedirect
-# from django.shortcuts import reverse
+from django.shortcuts import reverse
 from django.views import generic
 
-# from .models import Schedule
+from .models import Schedule
 
 
 class IndexView(generic.TemplateView):
     template_name = "reminder/index.html"
+
+
+def SaveSchedule(request):
+    c = Schedule(event_text=request.POST["text"])
+
+    if c != "":
+        c.save()
+    return HttpResponsePermanentRedirect(reverse("reminder:index"))
