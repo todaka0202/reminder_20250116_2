81dac2db7ceabdab95021c5d6f6701ff70fb13f4
todaka0202 <yizaihugao@gmail.com>
2025-01-23 15:09:09 +0900

    ；；

--- reminder/views.py
+++ reminder/views.py
@@ -10,18 +10,18 @@ from .models import Schedule
 
 class IndexView(generic.TemplateView):
     template_name = "reminder/index.html"
-    context_object_name = "scedule_title"
+    context_object_name = "schedule_title"
     model = Schedule
 
     def index(self):
-        scedule_title = Schedule.objects.order_by("schedule_text")
-        context = {"scesule_title": scedule_title}
+        schedule_title = Schedule.objects.order_by("schedule_text")
+        context = {"schedule_title": schedule_title}
         return render("reminder/index.html", context)
 
 
 def SaveSchedule(request):
     c = Schedule(schedule_text=request.POST["text"])
 
-    if c != "":
+    if c.schedule_text != "":
         c.save()
     return HttpResponsePermanentRedirect(reverse("reminder:index"))
